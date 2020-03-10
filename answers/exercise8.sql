94f22082d3c6c7b695bab4d14adf0718054c20a3
hlin <Zxsaqw12@&$>
2020-03-10 16:20:19 -0400

    Done Lab

--- answers/exercise8.sql
+++ answers/exercise8.sql
@@ -1,1 +1,15 @@
-CREATE SCHEMA Students
+CREATE TABLE Students (
+    ->  StudentName VARCHAR(100) NOT NULL,
+    ->  Address VARCHAR(150) NOT NULL,
+    ->  City VARCHAR(100) NOT NULL,
+    ->  PostalCode VARCHAR(50) NOT NULL,
+    ->  Country VARCHAR(150) NOT NULL
+    -> );
+
+INSERT INTO Students (
+    StudentName,
+    Address,
+    City,
+    PostalCode,
+    Country)
+VALUES ('Jane Doe', '57 Union St', 'Glasgow', 'Scotland', 'G13RB');
